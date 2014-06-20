95ed3c299f61ec3cd08deec9edc4cacc46e7e76e
Dave Longley <dlongley@digitalbazaar.com>
2014-06-20 17:24:10 -0400

    Rename files according to new naming conventions.

---
+++ slug-filter.js
@@ -0,0 +1,26 @@
+/*!
+ * Slug filter.
+ *
+ * Copyright (c) 2012-2014 Digital Bazaar, Inc. All rights reserved.
+ *
+ * @author Dave Longley
+ */
+define([], function() {
+
+'use strict';
+
+var deps = [];
+return {slug: deps.concat(factory)};
+
+function factory() {
+  return function(input) {
+    // replace spaces with dashes, make lower case and URI encode
+    if(input === undefined || input.length === 0) {
+      input = '';
+    }
+    return encodeURIComponent(
+      input.replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').toLowerCase());
+  };
+}
+
+});
